91910ef6dfbde4dba1c9d9ed64f283fffa8fc915
Arie <116137224+Ariemaharrucha@users.noreply.github.com>
2024-08-16 19:08:51 -0700

    Update main.ts

--- threads/src/main.ts
+++ threads/src/main.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(middleWareCheckorigin);
-app.use('/threads',ThreadRoutes);
+app.use('/',ThreadRoutes);
 
 mongoose.connect(process.env.MONGO_URI as string)
     .then(() => {
